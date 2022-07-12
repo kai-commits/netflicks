ee0b0388e50cc98405e9feb3ce57ebedb4e0391e
kai-commits <kaimeikle123@gmail.com>
2022-07-12 10:47:00 -0700

    add favourite icon to movies

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import React, { useEffect, useState } from 'react';
 
 export const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
+  const [like, setLike] = useState(false);
 
   useEffect(() => {
     axios.get(fetchURL).then((res) => {
@@ -26,6 +28,9 @@ export const Row = ({ title, fetchURL }) => {
               <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
                 <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
                   {item?.title}
+                </p>
+                <p>
+                  {like ? <FaHeart className='absolute top-4 left-4 text-gray-300' /> : <FaRegHeart className='absolute top-4 left-4 text-gray-300' />}
                 </p>
               </div>
             </div>
