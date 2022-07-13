47272844360d9a5fbec5b5902013d2104008517c
kai-commits <kaimeikle123@gmail.com>
2022-07-13 10:04:24 -0700

    create auth context provider

---
+++ src/context/AuthContext.js
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return unsubscribe();
+  });
+
+  return (
+    <AuthContextProvider value={{ signUp, logIn, logOut, user }}>
+      {children}
+    </AuthContextProvider>
+  );
+};
+
+export const UserAuth = () => {
+  return useContext(AuthContext);
+};
