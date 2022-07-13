49a641c2228c80cd0bd913635b5a6a31b2dd278b
kai-commits <kaimeikle123@gmail.com>
2022-07-13 09:55:17 -0700

    install and configure firebase

--- src/Requests.js
+++ src/Requests.js
@@ -1,4 +1,4 @@
-const key = process.env.REACT_APP_TMDB_API_KEY;
+const key = process.env.TMDB_API_KEY;
 
 export const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIRE_BASE_API_KEY,
+  authDomain: process.env.FIRE_BASE_AUTH_DOMAIN,
+  projectId: process.env.FIRE_BASE_PROJECT_ID,
+  storageBucket: process.env.FIRE_BASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIRE_BASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIRE_BASE_APP_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
