0485037d64735c00faa693f2a1382e4e5a21172b
kai-commits <kaimeikle123@gmail.com>
2022-07-21 13:56:58 -0700

    add account page and display saved shows

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { SavedShows } from '../components/SavedShows';
 
 export const Account = () => {
   return (
-    <div>Account</div>
-  )
-}
+    <>
+      <div className='w-full text-white'>
+        <img
+          className='w-full h-[400px] object-cover'
+          src='https://assets.nflxext.com/ffe/siteui/vlv3/1ef84595-1fdb-4404-adac-15215ceeb3ae/06253c65-f482-4924-8093-4bf36a7c63dc/CA-en-20220711-popsignuptwoweeks-perspective_alpha_website_large.jpg'
+          alt='/'
+        />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+};
