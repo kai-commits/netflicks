5ef20aec44b6d05b745b87a8673321b514312783
kai-commits <kaimeikle123@gmail.com>
2022-07-22 10:48:36 -0700

    fix movie title text wrap

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -48,7 +48,7 @@ export const SavedShows = () => {
                 alt={item?.title}
               />
               <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
+                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center whitespace-normal'>
                   {item?.title}
                 </p>
               </div>
